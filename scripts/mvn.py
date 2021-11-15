019f703bffb0a1626b21fc132c9f0fe323f644e8
hanada31 <hanada31@163.com>
2021-11-15 20:43:51 +0800

    update jarfile

--- scripts/mvn.py
+++ scripts/mvn.py
@@ -6,5 +6,5 @@ if __name__ == '__main__' :
     suffix = ""
     if len(sys.argv) == 2:
         suffix = "-"+sys.argv[1]
-    os.system("mvn -f pom.xml clean package")
-    shutil.copy("target/target/ICCBot-1.0-SNAPSHOT-jar-with-dependencies", "ICCBot"+suffix+".jar")
+    os.system("mvn -f pom.xml package")
+    shutil.copy("target/ICCBot-1.0-SNAPSHOT-jar-with-dependencies.jar", "ICCBot"+suffix+".jar")
