df8f3053b98ff480d7f4ffe51c04d488ed21d384
hanada31 <hanada31@163.com>
2021-11-16 21:30:10 +0800

    update jarfile

--- scripts/mvn.py
+++ scripts/mvn.py
@@ -6,5 +6,10 @@ if __name__ == '__main__' :
     suffix = ""
     if len(sys.argv) == 2:
         suffix = "-"+sys.argv[1]
-    os.system("mvn -f pom.xml package")
-    shutil.copy("target/ICCBot-1.0-SNAPSHOT-jar-with-dependencies.jar", "ICCBot"+suffix+".jar")
+    os.system("mvn -f pom.xml package -q")
+    if os.path.exists("target/ICCBot.jar"):
+        print("Successfully build! generate jar-with-dependencies in folder target/")
+        shutil.copy("target/ICCBot.jar", "ICCBot"+suffix+".jar")
+        print("copy jar to the root directory.")
+    else:
+        print("Fail to build! Please run \"mvn -f pom.xml package\" to see the detail info.")
