a861829d56a4ba0fda6595c43fe91e775dd68cb8
hanada31 <hanada31@163.com>
2021-12-15 11:00:04 +0800

    bug fix

--- scripts/runICCBot.py
+++ scripts/runICCBot.py
@@ -5,7 +5,7 @@ import shutil
 
 
 def analyzeApk(apkPath, resPath, sdk):
-
+    id=0
     logDir = resPath+"/logs"
     outputDir = resPath+"/output"
     if(not os.path.exists(logDir)): 
@@ -18,10 +18,12 @@ def analyzeApk(apkPath, resPath, sdk):
         extraArgs = ""# -noLibCode 
         for apk in apks:
             if apk[-4:] ==".apk":
-                print("java -jar "+jarFile+"  -path "+ apkPath +" -name "+apk+" -androidJar "+ sdk +"/platforms  "+ extraArgs +" -time 30 -maxPathNumber 100 -client MainClient  -outputDir "+outputDir+" >> "+logDir+"/"+apk[:-4]+".txt")
-                os.system("java -jar "+jarFile+"  -path "+ apkPath +" -name "+apk+" -androidJar "+ sdk +"/platforms "+ extraArgs +" -time 30 -maxPathNumber 100 -client MainClient -outputDir "+outputDir+" >> "+logDir+"/"+apk[:-4]+".txt")
-                print("java -jar ICCBot.jar  -path "+ apkPath +" -name "+apk+" -androidJar "+ sdk +"/platforms -time 30 -maxPathNumber 100 -client ToolEvaluateClient -outputDir "+outputDir+" >> "+logDir+"/"+apk[:-4]+".txt")
-                os.system("java -jar ICCBot.jar  -path "+ apkPath +" -name "+apk+" -androidJar "+ sdk +"/platforms -time 60 -maxPathNumber 100 -client ToolEvaluateClient -outputDir "+outputDir+" >> "+logDir+"/"+apk[:-4]+".txt")
+                id+=1
+                print ("\n\n\nThis is the "+str(id)+"th app " +apk)
+                resFile = logDir+"/"+apk[:-4]+".txt"
+                if(not os.path.exists(resFile)): 
+                    print("java -jar "+jarFile+"  -path "+ apkPath +" -name "+apk+" -androidJar "+ sdk +"/platforms  "+ extraArgs +" -time 30 -maxPathNumber 100 -client MainClient  -outputDir "+outputDir+" >> "+logDir+"/"+apk[:-4]+".txt")
+                    os.system("java -jar "+jarFile+"  -path "+ apkPath +" -name "+apk+" -androidJar "+ sdk +"/platforms "+ extraArgs +" -time 30 -maxPathNumber 100 -client MainClient -outputDir "+outputDir+" >> "+logDir+"/"+apk[:-4]+".txt")
 
 
 if __name__ == '__main__' :
@@ -29,13 +31,13 @@ if __name__ == '__main__' :
     resPath = sys.argv[2]
     jarFile = "ICCBot.jar"
     
-    # os.system("mvn -f pom.xml package -q")
-    # if os.path.exists("target/ICCBot.jar"):
-        # print("Successfully build! generate jar-with-dependencies in folder target/")
-        # shutil.copy("target/ICCBot.jar", jarFile)
-        # print("copy jar to the root directory.")
-    # else:
-        # print("Fail to build! Please run \"mvn -f pom.xml package\" to see the detail info.")
+    os.system("mvn -f pom.xml package -q")
+    if os.path.exists("target/ICCBot.jar"):
+        print("Successfully build! generate jar-with-dependencies in folder target/")
+        shutil.copy("target/ICCBot.jar", jarFile)
+        print("copy jar to the root directory.")
+    else:
+        print("Fail to build! Please run \"mvn -f pom.xml package\" to see the detail info.")
     
     sdk = "lib/"    
     analyzeApk(apkPath, resPath, sdk)
